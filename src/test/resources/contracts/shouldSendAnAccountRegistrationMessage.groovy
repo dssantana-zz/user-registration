0e516b34cdf3101710395c32da4880997f0bbe66
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-12-18 12:09:13 +0100

    Changing the assertion for the password

--- src/test/resources/contracts/shouldSendAnAccountRegistrationMessage.groovy
+++ src/test/resources/contracts/shouldSendAnAccountRegistrationMessage.groovy
@@ -18,7 +18,7 @@ Contract.make {
 			mobileNumber: "0452621048", 
 			ipAddress: "127.0.0.1",
 			birthday: "1979-10-16",
-			password: "Test01"
+			password: $(c("Test01"), p(regex(nonBlank())))
 			])		
 	}
 }
